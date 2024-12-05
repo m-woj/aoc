402edd36aba7a791469b88bd5f74ee14a4141ca1
Mateusz Wojciechowski <mwojciechowski296@gmail.com>
2024-12-05 20:30:15 +0100

    day 5

---
+++ 2024/5/main.py
@@ -0,0 +1,105 @@
+class Rule:
+    before: frozenset[int]
+    after: frozenset[int]
+
+    def __init__(self, before, after):
+        self.before = frozenset(before)
+        self.after = frozenset(after)
+
+    def __repr__(self):
+        return str(self.before) + " | " + str(self.after)
+
+
+class Task:
+    def __init__(self, input):
+        self.rules, self.manuals = self.get_rules_and_manuals(input)
+        self.manuals = self.get_valid_manuals()
+        self.sum = self.get_sum()
+
+    @staticmethod
+    def get_rules_and_manuals(input):
+        raw_rules = []
+        manuals = []
+
+        file = open(input, "r")
+        while line := file.readline():
+            if line != "\n":
+                raw_rules.append(line.strip())
+            else:
+                break
+
+        while line := file.readline():
+            manuals.append(line.strip())
+
+        file.close()
+
+        manuals = [manual.split(",") for manual in manuals]
+        for manual in manuals:
+            for i in range(len(manual)):
+                manual[i] = int(manual[i])
+
+        numbers = set()
+        for i in range(len(raw_rules)):
+            b, a = raw_rules[i].split("|")
+            raw_rules[i] = (int(b), int(a))
+            numbers.update(raw_rules[i])
+
+        rules = {}
+        for n in numbers:
+            before = []
+            after = []
+            for b, a in raw_rules:
+                if n == b:
+                    after.append(a)
+                elif n == a:
+                    before.append(b)
+
+            rule = Rule(before=before, after=after)
+            rules[n] = rule
+
+        return rules, manuals
+
+    def get_valid_manuals(self):
+        valid_manuals = []
+        for manual in self.manuals:
+            if self.is_valid_manual(manual):
+                valid_manuals.append(manual)
+
+        return valid_manuals
+
+    def is_valid_manual(self, manual):
+        for pos in range(len(manual)):
+            if not self.is_valid_pos(pos, manual):
+                return False
+
+        return True
+
+    def is_valid_pos(self, pos: int, manual):
+        n = manual[pos]
+        rule = self.rules.get(n)
+        if not rule:
+            return True
+
+        befores = manual[:pos]
+        for b in befores:
+            if b in rule.after:
+                return False
+
+        afters = manual[pos + 1 :]
+        for a in afters:
+            if a in rule.before:
+                return False
+
+        return True
+
+    def get_sum(self):
+        sum = 0
+        for manual in self.manuals:
+            val = manual[len(manual) // 2]
+            sum += val
+
+        return sum
+
+
+if __name__ == "__main__":
+    print(Task("input.txt").sum)
